6f3199870c97b3aea394426b9b13d589994cf73c
xX-Mill3r-Xx <millermartinsvasco1@gmail.com>
2022-02-27 20:13:22 -0300

    Initial201328022022

---
+++ JogoDaVelha_CSharp/Form1.Designer.cs
@@ -0,0 +1,315 @@
+﻿
+namespace JogoDaVelha_CSharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btn_1 = new System.Windows.Forms.Button();
+            this.btn_2 = new System.Windows.Forms.Button();
+            this.btn_3 = new System.Windows.Forms.Button();
+            this.btn_6 = new System.Windows.Forms.Button();
+            this.btn_5 = new System.Windows.Forms.Button();
+            this.btn_4 = new System.Windows.Forms.Button();
+            this.btn_9 = new System.Windows.Forms.Button();
+            this.btn_8 = new System.Windows.Forms.Button();
+            this.btn_7 = new System.Windows.Forms.Button();
+            this.lbl_PontoX = new System.Windows.Forms.Label();
+            this.lbl_PontoO = new System.Windows.Forms.Label();
+            this.lbl_Empate = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btn_Reset = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.btn_Reset);
+            this.panel1.Controls.Add(this.btn_9);
+            this.panel1.Controls.Add(this.btn_8);
+            this.panel1.Controls.Add(this.btn_7);
+            this.panel1.Controls.Add(this.btn_6);
+            this.panel1.Controls.Add(this.btn_5);
+            this.panel1.Controls.Add(this.btn_4);
+            this.panel1.Controls.Add(this.btn_3);
+            this.panel1.Controls.Add(this.btn_2);
+            this.panel1.Controls.Add(this.btn_1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(569, 360);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.lbl_Empate);
+            this.panel2.Controls.Add(this.lbl_PontoO);
+            this.panel2.Controls.Add(this.lbl_PontoX);
+            this.panel2.Location = new System.Drawing.Point(12, 378);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(569, 114);
+            this.panel2.TabIndex = 1;
+            // 
+            // btn_1
+            // 
+            this.btn_1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_1.Font = new System.Drawing.Font("Consolas", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_1.Location = new System.Drawing.Point(128, 24);
+            this.btn_1.Name = "btn_1";
+            this.btn_1.Size = new System.Drawing.Size(99, 99);
+            this.btn_1.TabIndex = 0;
+            this.btn_1.UseVisualStyleBackColor = true;
+            this.btn_1.Click += new System.EventHandler(this.btn_1_Click);
+            // 
+            // btn_2
+            // 
+            this.btn_2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_2.Font = new System.Drawing.Font("Consolas", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_2.Location = new System.Drawing.Point(233, 24);
+            this.btn_2.Name = "btn_2";
+            this.btn_2.Size = new System.Drawing.Size(99, 99);
+            this.btn_2.TabIndex = 1;
+            this.btn_2.UseVisualStyleBackColor = true;
+            this.btn_2.Click += new System.EventHandler(this.btn_1_Click);
+            // 
+            // btn_3
+            // 
+            this.btn_3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_3.Font = new System.Drawing.Font("Consolas", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_3.Location = new System.Drawing.Point(338, 24);
+            this.btn_3.Name = "btn_3";
+            this.btn_3.Size = new System.Drawing.Size(99, 99);
+            this.btn_3.TabIndex = 2;
+            this.btn_3.UseVisualStyleBackColor = true;
+            this.btn_3.Click += new System.EventHandler(this.btn_1_Click);
+            // 
+            // btn_6
+            // 
+            this.btn_6.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_6.Font = new System.Drawing.Font("Consolas", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_6.Location = new System.Drawing.Point(338, 129);
+            this.btn_6.Name = "btn_6";
+            this.btn_6.Size = new System.Drawing.Size(99, 99);
+            this.btn_6.TabIndex = 5;
+            this.btn_6.UseVisualStyleBackColor = true;
+            this.btn_6.Click += new System.EventHandler(this.btn_1_Click);
+            // 
+            // btn_5
+            // 
+            this.btn_5.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_5.Font = new System.Drawing.Font("Consolas", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_5.Location = new System.Drawing.Point(233, 129);
+            this.btn_5.Name = "btn_5";
+            this.btn_5.Size = new System.Drawing.Size(99, 99);
+            this.btn_5.TabIndex = 4;
+            this.btn_5.UseVisualStyleBackColor = true;
+            this.btn_5.Click += new System.EventHandler(this.btn_1_Click);
+            // 
+            // btn_4
+            // 
+            this.btn_4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_4.Font = new System.Drawing.Font("Consolas", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_4.Location = new System.Drawing.Point(128, 129);
+            this.btn_4.Name = "btn_4";
+            this.btn_4.Size = new System.Drawing.Size(99, 99);
+            this.btn_4.TabIndex = 3;
+            this.btn_4.UseVisualStyleBackColor = true;
+            this.btn_4.Click += new System.EventHandler(this.btn_1_Click);
+            // 
+            // btn_9
+            // 
+            this.btn_9.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_9.Font = new System.Drawing.Font("Consolas", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_9.Location = new System.Drawing.Point(338, 234);
+            this.btn_9.Name = "btn_9";
+            this.btn_9.Size = new System.Drawing.Size(99, 99);
+            this.btn_9.TabIndex = 8;
+            this.btn_9.UseVisualStyleBackColor = true;
+            this.btn_9.Click += new System.EventHandler(this.btn_1_Click);
+            // 
+            // btn_8
+            // 
+            this.btn_8.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_8.Font = new System.Drawing.Font("Consolas", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_8.Location = new System.Drawing.Point(233, 234);
+            this.btn_8.Name = "btn_8";
+            this.btn_8.Size = new System.Drawing.Size(99, 99);
+            this.btn_8.TabIndex = 7;
+            this.btn_8.UseVisualStyleBackColor = true;
+            this.btn_8.Click += new System.EventHandler(this.btn_1_Click);
+            // 
+            // btn_7
+            // 
+            this.btn_7.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_7.Font = new System.Drawing.Font("Consolas", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_7.Location = new System.Drawing.Point(128, 234);
+            this.btn_7.Name = "btn_7";
+            this.btn_7.Size = new System.Drawing.Size(99, 99);
+            this.btn_7.TabIndex = 6;
+            this.btn_7.UseVisualStyleBackColor = true;
+            this.btn_7.Click += new System.EventHandler(this.btn_1_Click);
+            // 
+            // lbl_PontoX
+            // 
+            this.lbl_PontoX.AutoSize = true;
+            this.lbl_PontoX.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lbl_PontoX.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lbl_PontoX.Font = new System.Drawing.Font("Consolas", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_PontoX.ForeColor = System.Drawing.Color.White;
+            this.lbl_PontoX.Location = new System.Drawing.Point(19, 39);
+            this.lbl_PontoX.Name = "lbl_PontoX";
+            this.lbl_PontoX.Size = new System.Drawing.Size(30, 33);
+            this.lbl_PontoX.TabIndex = 0;
+            this.lbl_PontoX.Text = "0";
+            // 
+            // lbl_PontoO
+            // 
+            this.lbl_PontoO.AutoSize = true;
+            this.lbl_PontoO.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lbl_PontoO.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lbl_PontoO.Font = new System.Drawing.Font("Consolas", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_PontoO.ForeColor = System.Drawing.Color.White;
+            this.lbl_PontoO.Location = new System.Drawing.Point(267, 39);
+            this.lbl_PontoO.Name = "lbl_PontoO";
+            this.lbl_PontoO.Size = new System.Drawing.Size(30, 33);
+            this.lbl_PontoO.TabIndex = 1;
+            this.lbl_PontoO.Text = "0";
+            // 
+            // lbl_Empate
+            // 
+            this.lbl_Empate.AutoSize = true;
+            this.lbl_Empate.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lbl_Empate.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lbl_Empate.Font = new System.Drawing.Font("Consolas", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Empate.ForeColor = System.Drawing.Color.White;
+            this.lbl_Empate.Location = new System.Drawing.Point(490, 39);
+            this.lbl_Empate.Name = "lbl_Empate";
+            this.lbl_Empate.Size = new System.Drawing.Size(30, 33);
+            this.lbl_Empate.TabIndex = 2;
+            this.lbl_Empate.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(2, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 23);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Pontuação X";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(217, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(131, 23);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Pontuação O";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(465, 19);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 23);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Empate";
+            // 
+            // btn_Reset
+            // 
+            this.btn_Reset.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn_Reset.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_Reset.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btn_Reset.FlatAppearance.BorderSize = 0;
+            this.btn_Reset.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Reset.Font = new System.Drawing.Font("Consolas", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Reset.ForeColor = System.Drawing.Color.White;
+            this.btn_Reset.Location = new System.Drawing.Point(469, 273);
+            this.btn_Reset.Name = "btn_Reset";
+            this.btn_Reset.Size = new System.Drawing.Size(75, 59);
+            this.btn_Reset.TabIndex = 9;
+            this.btn_Reset.Text = "Resetar";
+            this.btn_Reset.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(593, 504);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Jogo Da Velha";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btn_9;
+        private System.Windows.Forms.Button btn_8;
+        private System.Windows.Forms.Button btn_7;
+        private System.Windows.Forms.Button btn_6;
+        private System.Windows.Forms.Button btn_5;
+        private System.Windows.Forms.Button btn_4;
+        private System.Windows.Forms.Button btn_3;
+        private System.Windows.Forms.Button btn_2;
+        private System.Windows.Forms.Button btn_1;
+        private System.Windows.Forms.Label lbl_PontoO;
+        private System.Windows.Forms.Label lbl_PontoX;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbl_Empate;
+        private System.Windows.Forms.Button btn_Reset;
+    }
+}
+
---
+++ JogoDaVelha_CSharp/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JogoDaVelha_CSharp
+{
+    public partial class Form1 : Form
+    {
+        int palyerX = 0;
+        int palyerO = 0;
+        int empatePoint = 0;
+        int rodadas = 0;
+        bool FinalGame = false;
+        bool turn = true;
+
+        string[] texto = new string[9];
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_1_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            if(btn.Text == "" && FinalGame == false)
+            {
+                if (turn)
+                {
+                    btn.Text = "X";
+                    rodadas++;
+                    turn = !turn;
+                }
+                else
+                {
+                    btn.Text = "O";
+                    rodadas++;
+                    turn = !turn;
+                }
+            }
+            
+        }
+    }
+}
